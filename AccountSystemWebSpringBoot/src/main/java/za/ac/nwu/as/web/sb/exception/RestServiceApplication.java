559e1066636fe5d0981591d2059357b5c791ab01
Caitlyn Opperman <ceopperman2000@gmail.com>
2021-08-30 19:07:07 +0200

    Commit - Swagger Configuration

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/as/web/sb/exception/RestServiceApplication.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/as/web/sb/exception/RestServiceApplication.java
@@ -1,8 +1,10 @@
 package za.ac.nwu.as.web.sb.exception;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RestServiceApplication {
 
     public static void main(String[] args) {
